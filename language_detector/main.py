e41390a346c88224deee5985d266ce9b780c6a28
corginomics <anvle@ucdavis.edu>
2016-07-08 22:04:44 +0000

    Changes function naming conventions

--- language_detector/main.py
+++ language_detector/main.py
@@ -9,19 +9,19 @@ def detect_language(text, languages=LANGUAGES):
     numOfTimesLanguageOccurs = {}
 
     for word in text.split():
-        lang = whichLanguageIsIt(word, languages)
+        lang = which_language_is_it(word, languages)
         if lang is not None:
-            addToDictionary(lang, numOfTimesLanguageOccurs)
-    return getHighestOccurringLanguage(numOfTimesLanguageOccurs)
+            add_to_dictionary(lang, numOfTimesLanguageOccurs)
+    return get_most_occurring_language(numOfTimesLanguageOccurs)
 
-def whichLanguageIsIt(word, language_list ):
+def which_language_is_it(word, language_list ):
     for dict in language_list:
         if word in dict['common_words']: 
             return dict['name']
 
-def addToDictionary(lang, dictionary):
+def add_to_dictionary(lang, dictionary):
         if lang in dictionary: dictionary[lang] += 1
         else: dictionary[lang] = 1
 
-def getHighestOccurringLanguage(dictionary):
+def get_most_occurring_language(dictionary):
     return max(dictionary.keys(), key=(lambda key: dictionary[key]))
